d94a2afd82f24f1dd88c893783bb72507b374473
Max-dev-dev <codemagicmax@gmail.com>
2025-06-09 23:18:36 +0300

    dafv

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ios_f_n_splashbigdiscovery_3259/cubit/my_fish_cubit.dart';
 import 'package:ios_f_n_splashbigdiscovery_3259/pages/splash_screen.dart';
+import 'package:ios_f_n_splashbigdiscovery_3259/ver_screen.dart';
 
-void main() {
-  runApp(const MainApp());
+class AppConstants {
+  static const String oneSignalAppId = "997c8960-5db5-4c8d-ab72-3e45da9f3dc4";
+  static const String appsFlyerDevKey = "v7xCW2oiGJ5JauPXwWiS5W";
+  static const String appID = "6743842613";
+
+  static const String baseDomain = "astonishing-high-ranking-blissfulness.space";
+  static const String verificationParam = "yQqcejSE";
+
+  static const String splashImagePath = 'assets/images/splash_background.png';
 }
 
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final now = DateTime.now();
+  final dateOff = DateTime(2024, 6, 11, 20, 00);
+
+  final initialRoute = now.isBefore(dateOff) ? '/white' : '/verify';
+  runApp(RootApp(
+    initialRoute: initialRoute,
+    whiteScreen: MainApp(),
+  ));
+}
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
